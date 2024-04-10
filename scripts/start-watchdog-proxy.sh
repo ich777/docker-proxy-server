f79c9ce4320564e17a86b72bcdfddf9db44d027f
Christoph Hummer <christophhummer@gmail.com>
2024-04-10 16:47:00 +0200

    scripts/start-watchdog-proxy.sh aktualisiert

--- scripts/start-watchdog-proxy.sh
+++ scripts/start-watchdog-proxy.sh
@@ -4,7 +4,7 @@ killpid="$(pidof proxy)"
 while true
 do
 	tail --pid=$killpid -f /dev/null
-	kill -SIGTERM $(pidof tail)
+	kill -SIGTERM $(pidof sleep)
     echo "---Proxy crashed, restarting container!---"
 	exit 0
 done

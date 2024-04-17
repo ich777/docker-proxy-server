f442059445352037eea402f20af0638afd495dc3
Christoph Hummer <christophhummer@gmail.com>
2024-04-17 14:15:27 +0200

    scripts/start-connected-containers.sh aktualisiert

--- scripts/start-connected-containers.sh
+++ scripts/start-connected-containers.sh
@@ -6,23 +6,15 @@ if [ -z "${CONNECTED_CONTAINERS%%:*}" ] || [ -z "${CONNECTED_CONTAINERS#*:}" ] |
   exit 1
 fi
 
-# Wait 5 seconds to start the connection and try to connect 5 times
+# Wait 5 seconds to start the connection
 sleep 5
 echo "---Starting connected containers watchdog on ${CONNECTED_CONTAINERS}---"
-while [ $tries -lt 5 ]; do
-  nc ${CONNECTED_CONTAINERS%%:*} ${CONNECTED_CONTAINERS#*:}
-  if [ $? -eq 0 ]; then
-    break
-  else
-    echo "---Connection attempt $((tries + 1)) failed, rytring in 5 seconds..."
-    tries=$((tries + 1))
-    sleep 5
-  fi
-done
+nc ${CONNECTED_CONTAINERS%%:*} ${CONNECTED_CONTAINERS#*:}
+EXIT_STATUS=$?
 
 # Determin on exit status if connection was successfull or if container should be restarted
-if [ $tries -eq 5 ]; then
-  echo "---Couldn't connect to: ${CONNECTED_CONTAINERS%%:*} on port: ${CONNECTED_CONTAINERS#*:} after 5 tries!---"
+if [ "${EXIT_STATUS}" != 0 ]; then
+  echo "---Couldn't connect to: ${CONNECTED_CONTAINERS%%:*} on port: ${CONNECTED_CONTAINERS#*:}"
   exit 1
 else
   echo "---Connection to connected container: ${CONNECTED_CONTAINERS} lost, restarting in 10 seconds...---"
